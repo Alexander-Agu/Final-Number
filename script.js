737835b5826481cf5ddd1169d918a9f6f138666c
Alexander Agu <theonlyagu@gmail.com>
2024-06-19 17:35:58 +0200

    Finished making my header responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+// Sidebar responsive for mobile functionality
+
+// Opens the side bar
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar-list');
+
+    sidebar.style.display = 'flex'
+}
+// Hides the side bar
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar-list');
+
+    sidebar.style.display = 'none'
+}
