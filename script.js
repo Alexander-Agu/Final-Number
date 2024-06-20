6df23a3e90bc6da8a0c3ff37fda4610f87e70088
Alexander Agu <theonlyagu@gmail.com>
2024-06-20 08:51:57 +0200

    Made the submit button to be able to iterate through all the challenges

--- script.js
+++ script.js
@@ -44,12 +44,23 @@ const numbers = [
     {
         finalNumber: 7,
         numChoices: [
-            {firstNum: 3},
-            {seconNum: 5},
-            {thirdNum: 9},
-            {fouthNum: 4},
-            {fithNum: 8},
-            {sixthNum: 3}
+            {firstNum: '3'},
+            {seconNum: '5'},
+            {thirdNum: '9'},
+            {fouthNum: '4'},
+            {fithNum: '8'},
+            {sixthNum: '3'}
+        ],
+    },
+    {
+        finalNumber: 7,
+        numChoices: [
+            {firstNum: '6'},
+            {seconNum: '8'},
+            {thirdNum: '1'},
+            {fouthNum: '2'},
+            {fithNum: '7'},
+            {sixthNum: '1'}
         ],
     },
 
@@ -63,12 +74,14 @@ function displaySymbol(n){
 }
 
 // A function that display the number values on the input 
+// It also handles the submit button
 let currentChallengeIndex = 0;
 let score = 0
 function displayNumber(){
     let currentChallenge = numbers[currentChallengeIndex]
 
     console.log(currentChallenge)
+    // Accessing the values in the numbers array object
     let one = currentChallenge.numChoices[0].firstNum
     let two = currentChallenge.numChoices[1].seconNum
     let three = currentChallenge.numChoices[2].thirdNum
@@ -76,6 +89,7 @@ function displayNumber(){
     let five = currentChallenge.numChoices[4].fithNum
     let six = currentChallenge.numChoices[5].sixthNum
 
+    // Displays the numchoices numbers on the buttons
     buttonOne.textContent = one
     buttonTwo.textContent = two
     buttonThree.textContent = three
@@ -84,6 +98,8 @@ function displayNumber(){
     buttonSix.textContent = six
     
 
+    // Click event for each button to display their value on the input screen
+    // Make it in a way that each number can only be selected once
     buttonOne.onclick = ()=>{
         displayer.value += one
     }
@@ -104,31 +120,67 @@ function displayNumber(){
     }
 
 
-    document.getElementById("submit").onclick = ()=>{
-        total = eval(displayer.value)
-        currentChallenge
-        if (currentChallengeIndex < numbers.length){
+    console.log(currentChallengeIndex)
+    console.log(numbers.length -1)
+    if (currentChallengeIndex < numbers.length - 1){        // If statements that handles the submit button when the currentChallengeIndex is less than numbers.lenth - 1
+
+        document.getElementById("submit").onclick = ()=>{
+            currentChallengeIndex++;    // Everytime the submit button is clicked it will increment currentChallengeIndex
+
+            displayNumber()     // Using recursion so we can be able to iterate through all the properties of numbers using the currentChallengeIndex incrementation
+            total = eval(displayer.value)   
             if (total === currentChallenge.finalNumber){
-                currentChallengeIndex++;
                 score++;
                 displayer.value = ""
-                displayNumber();
+            }
+    
+            else{
+                alert(`I was looking for FINAL NUMBER:${currentChallenge.finalNumber} you gave me ${total}`)
+                displayer.value = ""
+            }
+        }
+    }
+
+    else if( currentChallengeIndex == numbers.length - 1){  // Base case for the ending of Recursion
+        
+        document.getElementById("submit").onclick = ()=>{
+            currentChallengeIndex++;
+            total = eval(displayer.value)
+            if (total === currentChallenge.finalNumber){
+                score++;
+                displayer.value = ""
+                buttonOne.textContent = ''
+            }
+    
+            else{
+                alert(`I was looking for FINAL NUMBER:${currentChallenge.finalNumber} you gave me ${total}`)
+                displayer.value = ""
+                buttonOne.textContent = ''
+            }
+        }
+    }
+/*     document.getElementById("submit").onclick = ()=>{
+        total = eval(displayer.value)
+        displayNumber()
+        if (currentChallengeIndex <= numbers.length){
+            if (total === currentChallenge.finalNumber){
+                score++;
+                displayer.value = ""
             }
 
             else{
                 alert(`I was looking for FINAL NUMBER:${currentChallenge.finalNumber} you gave me ${total}`)
-                currentChallengeIndex++;
-                score--;
                 displayer.value = ""
-                displayNumber();
             }
         }
 
         else{
             console.log("Game Over")
         }
-    }
+    } */
+    
     console.log(score);
+    return
 
 
     
